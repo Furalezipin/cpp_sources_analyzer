79b58f55803a1d006db7f2ac128fba05664d14c2
Alex Popov <furalezipin@mail.ru>
2023-01-15 18:13:54 +0300

    Реализован парсинг заголовков, обработка закомментированных строк (примитивно)

--- sources/include_guard/sourcefile.cpp
+++ sources/include_guard/sourcefile.cpp
@@ -1,9 +1,28 @@
 #include "sourcefile.h"
 
+/*
+#include "sourcefile.h"
+#include "sourcefile.h"
+#include "sourcefile.h"
+#include "sourcefile.h"
+#include "sourcefile.h"
+#include "sourcefile.h"
+#include "sourcefile.h"
+#include "sourcefile.h"
+
+*/
+
 #include <fstream>
 #include <iostream>
 
 #include <boost/algorithm/string.hpp>
+
+constexpr auto INCLUDE_DIRECTIVE = "#include" ;
+constexpr auto COMMENTARY = "//";
+constexpr auto MULTIROW_COMMENTARY_BEGIN = "/*";
+constexpr auto MULTIROW_COMMENTARY_END = "*/";
+
+using namespace boost::algorithm;
 
 SourceFile::SourceFile(const std::string& path) {
 	
@@ -11,16 +30,49 @@ SourceFile::SourceFile(const std::string& path) {
     std::ifstream in(path);
 
     if (in.is_open())
-    {
+    {   
+
+        bool inCommentaryBlock = false;
+
         while (getline(in, line))
-        {
-            std::cout << line << std::endl;
+        {   
+            trim(line);
+
+            if (starts_with(line, MULTIROW_COMMENTARY_BEGIN)) inCommentaryBlock = true;
+
+            if (starts_with(line, MULTIROW_COMMENTARY_END)) inCommentaryBlock = false;
+
+            if (inCommentaryBlock || starts_with(line, COMMENTARY)) continue;
+
+            if (starts_with(line, INCLUDE_DIRECTIVE)) {
+                auto it = std::find(line.begin(), line.end(), '<');
+                if (it != line.end()) {
+                    auto it2 = std::find(it, line.end(), '>');
+                    m_listGlobalIncludes.emplace_back(++it, it2);
+                }
+                else {
+                    auto it1 = std::find(line.begin(), line.end(), '"');;
+                    auto it2 = std::find(++it1, line.end(), '"');
+                    m_listLocalIncludes.emplace_back(it1, it2);
+                }
+            };
         }
     }
     else {
         std::cerr << "Cannot open file " << path << "\n";
     }
     in.close();
+
+    std::cout << "GLOBAL: " << std::endl;
+    for (auto& s : m_listGlobalIncludes) {
+        std::cout << s << std::endl;
+    }
+
+    std::cout << "\n" << std::endl;
+    std::cout << "LOCAL: " << std::endl;
+    for (auto& s : m_listLocalIncludes) {
+        std::cout << s << std::endl;
+    }
 
 }
 
